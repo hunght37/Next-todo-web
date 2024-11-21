5fa50371354255d5b3d618543a34000119153af8
devvkylejack@gmail.com <devvKyleJack@gmail.com>
2024-11-22 00:46:15 +0700

    fix: add DATABASE_URL to next.config.js with fallback value

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   webpack: (config) => {
     config.externals = [...config.externals, 'mongodb'];
     return config;
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "mongodb+srv://Teseter1:4oNjmcc4jmoPrgj8@cluster0.slzq6.mongodb.net/todo_db?retryWrites=true&w=majority"
   }
 };
 
